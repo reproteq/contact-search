a7515146545cacb1fbdfd8c81dcf5ecb67b563ee
reproteq <reproteq@gmail.com>
2019-12-01 23:15:24 +0100

    example modal

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,7 @@ class ContactsController < ApplicationController
   # GET /contacts.json
   def index
     @contacts = Contact.all
+    
   end
 
   # GET /contacts/1
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+ # match ':controller(/:action(/:id))', :via => [:get, :post]
+ # get 'contact/delete'
+#  get 'contact/destroy'
+
   resources :emails
   resources :phones
   resources :contacts
   get 'main/index'
   get 'search/create'
   root to: 'main#index'
+ # get "/main", to: redirect("/contacts")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
